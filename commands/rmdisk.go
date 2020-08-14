6841db8341062ffe5ec5e86eefc0b8cedb47dee3
Francisco Suarez <39706929+Yosoyfr@users.noreply.github.com>
2020-08-13 18:51:18 -0600

    [RMDISK] Eliminar un disco duro terminado y funcionando

--- commands/rmdisk.go
+++ commands/rmdisk.go
@@ -8,15 +8,15 @@ import (
 
 //Funcion para eliminar un archivo que represente un disco duro
 func RMDisk(path string) {
-	err := os.Remove(path)
-	if err != nil {
-		log.Fatal(err)
-	}
 	fmt.Println("Desea eliminar este disco duro?")
-	fmt.Println("0 - Concelar \n 1 - Confirmar")
+	fmt.Println("0 - Cancelar\n1 - Confirmar")
 	var input int
 	fmt.Scanln(&input)
 	if input == 1 {
+		err := os.Remove(path)
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Println("Disco Eliminado con exito!!")
 	} else {
 		fmt.Println("Cancelado")
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 //Funcion Main
 func main() {
 	fmt.Println("Prueba de creacion de un disco ----------")
-	commands.MKDisk("disc_2.dsk", 18, 0)
-	commands.ReadFile("disc_2.dsk")
+	commands.MKDisk("disc_3.dsk", 25, 1)
+	commands.ReadFile("disc_3.dsk")
+	commands.RMDisk("disc_2.dsk")
 }
